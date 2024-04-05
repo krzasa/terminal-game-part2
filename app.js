28a6686eedaea19977df9208d0e758dbaa990969
krzasa <krzasa96@gmail.com>
2024-04-05 17:14:14 -0400

    Added the inputCheck function

--- app.js
+++ app.js
@@ -26,58 +26,66 @@ const container = document.querySelector('.gameboard')
  let gameDirection = "This is test text "
 
  let inputVal = ""
- const inputCount = 0
-/**---------------------------------FUNCTIONS------------------------------------------- */
-const userCall = (name) => {  // creating a function for asking the name of the players 
-    
-   
-
-    player1.name =  name // create variable for player 1 from user prompt
-    gameDirection = `Player 1's name is ${player1.name}`;
-
-    player2.name = prompt('What is your name Player 2? ');  // create variable for player 2 prompt 
-    console.log(`Player 2's name is ${player2.name}`);
-
-}
-
-const inputCheck = (input) => {
-    if (inputVal.length > 1 && inputCount < 1) {
-        player1.name = input
-        gameDirection = `Player 1's name is ${player1.name}`;
-        inputCount += 1
-    }else if ( inputVal.length > 1 && inputCount < 2){
-        player2.name = input
-        gameDirection = `Player 2's name is ${player2.name}`;
-        inputCount += 1
-    }
-    if (inputVal.length < 2 && inputCount < 3) {
-        if (input ==="X"|| input ==="x"){
-        player1.symbol = "X"
-        inputCount += 1
-        } else if (input ==="O"|| input ==="o"){
-        player1.symbol = "O"
-        inputCount += 1
-        }
-    } else if (inputVal.length < 2 && inputCount < 4){
-        if (player1.symbol==="X"){
-            player2.symbol = "O"
-
-        } else if (player1.symbol === "O") {
-            player2.symbol = "X"
-        }
-
-    }
-
-
-
-}
-
-/**------------------------------------------------------Event Listeners/calls-------------------------------------- */
-const gameboardElement = document.querySelector(".gameboard");
+ let inputCount = 0
+ const gameboardElement = document.querySelector(".gameboard");
 const gameButtonElement = document.querySelector(".gameButton")
 const direcButtonElement = document.querySelector('.directionsButton')
 const submitButton = document.querySelector(".submit")
 const choiceInput = document.querySelector('.textInput')
+ let directionElement = document.querySelector(".directions")
+/**---------------------------------FUNCTIONS------------------------------------------- */
+// const userCall = (name) => {  // creating a function for asking the name of the players 
+    
+   
+
+//     player1.name =  name // create variable for player 1 from user prompt
+//     gameDirection = `Player 1's name is ${player1.name}`;
+
+//     player2.name = prompt('What is your name Player 2? ');  // create variable for player 2 prompt 
+//     console.log(`Player 2's name is ${player2.name}`);
+
+// }
+
+const inputCheck = (input) => {
+    //console.log(input); // checks the function input
+    if (input.length > 1 && inputCount < 1) {
+        player1.name = input
+        gameDirection = `Player 1's name is ${player1.name}`;
+        //console.log(gameDirection);
+        directionElement.textContent = gameDirection
+        
+        inputCount += 1
+    }else if ( input.length > 1 && inputCount < 2){
+        player2.name = input
+        gameDirection = `Player 2's name is ${player2.name} , now ${player1.name} what symbol do you want?`;
+        directionElement.textContent = gameDirection
+        inputCount += 1
+    }
+    if (input.length < 2) {
+        
+        
+        if (input === "X" || input === "x"){
+        player1.symbol = "X"
+        player2.symbol = "O"
+        
+        // console.log(player1.symbol);
+        gameDirection = `${player1.name} 's symbol is ${player1.symbol} , and ${player2.name} is ${player2.symbol}`
+        directionElement.textContent = gameDirection
+        inputCount += 1
+        } 
+        else if (input ==="O"|| input ==="o"){
+        player1.symbol = "O"
+        player2.symbol = "X"
+        gameDirection = `${player1.name} 's symbol is ${player1.symbol} , and ${player2.name} is ${player2.symbol}`
+        directionElement.textContent = gameDirection
+        inputCount += 1
+        }
+       
+    } 
+}
+
+/**------------------------------------------------------Event Listeners/calls-------------------------------------- */
+
 
 gameButtonElement.addEventListener('click', () =>{  
     while(gameboardElement.firstChild) { 
@@ -89,7 +97,7 @@ gameButtonElement.addEventListener('click', () =>{
     const para2 = document.createElement("h1");
     const para3 = document.createElement("h1");
     const para4 = document.createElement ('h2')
-    gameDirection ="Please enter the name of Player 1 in the input box"
+    gameDirection ="Please enter the name of Player 1 in the input box, then player 2's name after submitting"
     if (player1.name === "") {
 
     }
@@ -100,13 +108,14 @@ gameButtonElement.addEventListener('click', () =>{
     para2.textContent = textMaker2
     para3.textContent = textMaker3 
     para4.textContent = gameDirection
+    para4.setAttribute("class","directions")
     para.style.wordSpacing = "10px"
     para2.style.wordSpacing = "10px"
     para3.style.wordSpacing = "10px"
     gameboardElement.appendChild(para);
     gameboardElement.appendChild(para2);
     gameboardElement.appendChild(para3);
-    gameboardElement.appendChild(para4);
+    directionElement.appendChild(para4);
 
 
     
@@ -127,12 +136,14 @@ direcButtonElement.addEventListener('click', () =>{
     
 })
 
-submitButton.addEventListener('click', () =>{  
+submitButton.addEventListener('click', () =>{ 
+    // console.log(choiceInput.value); 
     if(choiceInput.value != ""){
         inputCheck(choiceInput.value)
+        // console.log("inputCheck(fire)");
     }
     
-   
+    
     
     
 })
